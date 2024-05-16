4cce5f7cd6ddea0a613ea2f718d322f9f904ead4
doop <flavio.oderzo@etu.univ-amu.fr>
2024-05-16 11:01:00 +0200

    feat : exo 7

--- src/main/java/fr/amu/iut/exercice7/CounterController.java
+++ src/main/java/fr/amu/iut/exercice7/CounterController.java
@@ -1,22 +1,40 @@
 package fr.amu.iut.exercice7;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CounterController implements Initializable {
 
     int counter = 0;
+    @FXML
+    private Label counterLabel;
+    @FXML
+    private Button decrementButton;
+    @FXML
+    private Button incrementButton;
 
     public void increment() {
+        ++counter;
+        counterLabel.setText("" + counter);
     }
 
     public void decrement() {
+        --counter;
+        counterLabel.setText("" + counter);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Initializing CounterController...");
+        counterLabel.setText("0");
    }
 }
