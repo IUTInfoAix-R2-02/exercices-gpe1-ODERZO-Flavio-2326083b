ba7266cb35b0590dc33aa2b578ff95c9086b3e73
doop <flavio.oderzo@etu.univ-amu.fr>
2024-05-23 14:22:16 +0200

    feat : exo 13

--- src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
+++ src/main/java/fr/amu/iut/exercice13/MainPersonnes.java
@@ -1,5 +1,6 @@
-package fr.amu.iut.exercice3;
+package fr.amu.iut.exercice13;
 
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -10,14 +11,60 @@ public class MainPersonnes  {
     private static ObservableList<Personne> lesPersonnes;
 
     private static ListChangeListener<Personne> unChangementListener;
+    private static ListChangeListener<Personne> plusieursChangementListener;
 
     public static void main(String[] args) {
 
-        lesPersonnes = FXCollections.observableArrayList();
+        lesPersonnes = FXCollections.observableArrayList(personne -> new Observable[] {
+                personne.ageProperty()
+        });
 
-//        unChangementListener = à completer
+        unChangementListener = new ListChangeListener<Personne>() {
+            @Override
+            public void onChanged(Change<? extends Personne> change) {
+                change.next();
+                if (change.wasAdded()){
+                    System.out.println(change.getAddedSubList().get(0).getNom());
+                } else if (change.wasRemoved()){
+                    System.out.println(change.getRemoved().get(0).getNom());
+                } else if (change.wasUpdated()){
+                    System.out.println(lesPersonnes.get(change.getFrom()).getNom() + " a maintenant "
+                            + lesPersonnes.get(change.getFrom()).getAge() + " ans");
+                }
+            }
+        };
 
         lesPersonnes.addListener(unChangementListener);
+
+        plusieursChangementListener = new ListChangeListener<Personne>() {
+            @Override
+            public void onChanged(Change<? extends Personne> change) {
+                while (change.next()) {
+                    if (change.wasAdded()){
+                        for (Personne p : change.getAddedSubList()) {
+                            System.out.println(p.getNom() + " ajouté");
+                        }
+                    } else if (change.wasRemoved()){
+                        for (Personne p : change.getRemoved()) {
+                            System.out.println(p.getNom() + " supprimé");
+                        }
+                    } else if (change.wasUpdated()){
+                        for (int i = change.getFrom(); i < change.getTo(); i++) {
+                            System.out.println(lesPersonnes.get(change.getFrom()).getNom() + " a maintenant "
+                                    + lesPersonnes.get(change.getFrom()).getAge() + " ans");
+                        }
+                    }
+                }
+                System.out.println("fini");
+            }
+        };
+
+        //question1();
+        //question2();
+        //question3();
+        lesPersonnes.removeListener(unChangementListener);
+        lesPersonnes.addListener(plusieursChangementListener);
+        question5();
     }
 
     public static void question1() {
