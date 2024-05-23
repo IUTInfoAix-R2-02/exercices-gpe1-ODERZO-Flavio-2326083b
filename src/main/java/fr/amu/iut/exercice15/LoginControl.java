52217ad3a06d505db752cefcd8fdc66e2aa4b4d8
doop <flavio.oderzo@etu.univ-amu.fr>
2024-05-23 17:00:43 +0200

    feat : exo 15

--- src/main/java/fr/amu/iut/exercice15/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice15/LoginControl.java
@@ -1,6 +1,17 @@
-package fr.amu.iut.exercice5;
+package fr.amu.iut.exercice15;
 
+import javafx.beans.binding.Binding;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.binding.When;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -12,8 +23,61 @@ public class LoginControl extends GridPane {
     @FXML
     private PasswordField pwd;
 
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    private Button okBtn;
+
+    @FXML
+    public void initialize(){
+        createBindings();
+    }
     private void createBindings() {
-        // MÉTHODE À COMPLÉTER
+        // mot de passe ineditable quand userId.length < 6
+        BooleanBinding userIdMoinsDe6 = userId.textProperty().length().lessThan(6);
+
+        When quandUserIdMoinsDe6Char = new When(userIdMoinsDe6);
+        pwd.editableProperty().bind(quandUserIdMoinsDe6Char.then(false).otherwise(true));
+
+        // cancel non utilisable si les deux champs sont vides
+        BooleanBinding userIdVide = userId.textProperty().isEmpty();
+        BooleanBinding mdpVide = pwd.textProperty().isEmpty();
+
+        When quandChampsVides = new When(Bindings.and(
+                userIdVide,
+                mdpVide
+        ));
+
+        cancelBtn.disableProperty().bind(quandChampsVides.then(true).otherwise(false));
+
+        // ok non utilisable si :
+        // - mdp moins de 8 char
+        // - pas de maj
+        // - pas de chiffre
+
+        BooleanBinding moinsDe8Char = pwd.textProperty().length().lessThan(8);
+        BooleanProperty pasDeMaj = new SimpleBooleanProperty(true);
+        BooleanProperty pasDeChiffre = new SimpleBooleanProperty(true);
+        ChangeListener<String> pwdChangeListener = new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String ancien, String nouveau) {
+                if (nouveau.matches(".*[0-9].*")) {
+                    pasDeChiffre.set(false);
+                }
+                else pasDeChiffre.set(true);
+
+                if (nouveau.matches(".*[A-Z].*")) {
+                    pasDeMaj.set(false);
+                }
+                else pasDeMaj.set(true);
+            }
+        };
+        pwd.textProperty().addListener(pwdChangeListener);
+        BooleanBinding pasDeMajOuPasDeChiffre = Bindings.or(pasDeMaj, pasDeChiffre);
+        When mdpNeRespectePasConditions = new When(Bindings.or(pasDeMajOuPasDeChiffre, moinsDe8Char));
+
+        okBtn.disableProperty().bind(mdpNeRespectePasConditions.then(true).otherwise(false));
     }
 
     @FXML
